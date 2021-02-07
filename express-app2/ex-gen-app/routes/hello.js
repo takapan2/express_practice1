2960b071d07eebf7ebb4ee4b48f9f5be2e573436
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-02-07 22:33:45 +0900

    list4-14

--- express-app2/ex-gen-app/routes/hello.js
+++ express-app2/ex-gen-app/routes/hello.js
@@ -2,11 +2,18 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    var name=req.query.name;
-    var mail=req.query.mail;
     var data={
         title:'Hello!',
-        content:'あなたの名前は、'+name+'。<br>'+'メールアドレスは、'+mail+'です。',
+        content:'※何か書いて送信してください。',
+    };
+    res.render('hello',data);
+});
+
+router.post('/post',(req,res,next)=>{
+    var msg=req.body['message'];
+    var data={
+        title:'Hello!',
+        content:'あなたは、「'+msg+'」と送信しました。'
     };
     res.render('hello',data);
 });
