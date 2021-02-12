94e2591ae420248e20b53f3d8b6166dc82d782f4
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-02-13 03:41:18 +0900

    list5-6修正

--- express-app2/ex-gen-app/routes/hello.js
+++ express-app2/ex-gen-app/routes/hello.js
@@ -24,4 +24,23 @@ router.get('/',(req,res,next)=>{
     });
 });
 
+router.get('/add',(req,res,next)=>{
+    var data={
+        title:'Hello/Add',
+        content:'新しいレコードを入力:'
+    };
+    res.render('hello/add',data);
+});
+
+router.post('/add',(req,res,next)=>{
+    const nm=req.body.name;
+    const ml=req.body.mail;
+    const ag=req.body.age;
+
+    db.serialize(()=>{
+        db.run('insert into mydata (name,mail,age) values (?,?,?)',nm,ml,ag);
+    });
+    res.redirect('/hello');
+})
+
 module.exports=router;
