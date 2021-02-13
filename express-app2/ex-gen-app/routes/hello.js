8a374f91f892b12deca51903fe3102028780cd42
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-02-13 21:53:52 +0900

    list5-13

--- express-app2/ex-gen-app/routes/hello.js
+++ express-app2/ex-gen-app/routes/hello.js
@@ -89,4 +89,30 @@ router.post('/edit',(req,res,next)=>{
     res.redirect('/hello');
 });
 
+router.get('/delete',(req,res,next)=>{
+    const id=req.query.id;
+    db.serialize(()=>{
+        const q="select * from mydata where id = ?";
+        db.get(q,[id],(err,row)=>{
+            if(!err){
+                var data={
+                    title:'Hello/Delete',
+                    content:'id='+id+'のレコードの削除：',
+                    mydata:row,
+                };
+                res.render('hello/delete',data);
+            }
+        });
+    });
+});
+
+router.post('/delete',(req,res,next)=>{
+    const id=req.body.id;
+    db.serialize(()=>{
+        const q="delete from mydata where id = ?";
+        db.run(q,id);
+    });
+    res.redirect('/hello');
+});
+
 module.exports=router;
