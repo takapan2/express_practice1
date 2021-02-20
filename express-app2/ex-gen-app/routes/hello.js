83f5e34f981431527dc6bc4d2a93f1bff5b4e98a
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-02-20 23:54:11 +0900

    list 5-last

--- express-app2/ex-gen-app/routes/hello.js
+++ express-app2/ex-gen-app/routes/hello.js
@@ -38,6 +38,9 @@ router.post('/add',[
         check('name','NAMEは必ず入力してください。').notEmpty().escape(),
         check('mail','MAILはメールアドレスを記入してください。').isEmail().escape(),
         check('age','AGEは年齢（整数）を入力してください。').isInt(),
+        check('age','AGEはゼロ以上120以下で入力してください。').custom(value =>{
+            return value >= 0 & value <= 120;
+        }),
     ],(req,res,next)=>{
         const errors=validationResult(req);
 
