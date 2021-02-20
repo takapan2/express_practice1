c1e3a7b4fa27596f1372dfd76771dd4372a39359
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-02-21 00:40:29 +0900

    list 6-10

--- express-app2/ex-gen-app/routes/users.js
+++ express-app2/ex-gen-app/routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const db=require('../models/index');
+const {Op}=require("sequelize");
 
 router.get('/',(req,res,next)=>{
   const id=req.query.id;
   db.User.findAll({
     where:{
-      id:id
+      id:{[Op.lte]:id}
     }
   }).then(usrs=>{
     var data={
